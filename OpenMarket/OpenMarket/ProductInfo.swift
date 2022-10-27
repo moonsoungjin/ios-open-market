e58235eb03e0b6f937ebd5a81d9b70b9e1911835
parkseyoon <issou06261@gmail.com>
2022-10-27 13:49:40 +0900

    fix: vendorName 추가 및 프로퍼티 타입 변경

--- OpenMarket/OpenMarket/ProductInfo.swift
+++ OpenMarket/OpenMarket/ProductInfo.swift
@@ -10,28 +10,30 @@ import Foundation
 struct ProductInfo: Decodable {
     var id: Int
     var vendorId: Int
+    var vendorName: String
     var name: String
     var description: String
     var thumbnail: String
     var currency: String
-    var price: Int
-    var bargainPrice: Int
-    var discountedPrice: Int
+    var price: Double
+    var bargainPrice: Double
+    var discountedPrice: Double
     var stock: Int
     var createdAt: String
     var issuedAt: String
     
     enum CodingKeys: String, CodingKey {
         case id
+        case vendorId = "vendor_id"
+        case vendorName
         case name
         case description
         case thumbnail
         case currency
         case price
-        case stock
-        case vendorId = "vendor_id"
         case bargainPrice = "bargain_price"
         case discountedPrice = "discounted_price"
+        case stock
         case createdAt = "created_at"
         case issuedAt = "issued_at"
     }
