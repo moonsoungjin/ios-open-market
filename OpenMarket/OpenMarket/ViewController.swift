a4f1b3fbe041a9637e4dff1fba6974dce685a8ac
parkseyoon <issou06261@gmail.com>
2022-11-07 11:37:52 +0900

    remove: 파일 구조 변경으로 인한 불필요한 코드 제거

--- OpenMarket/OpenMarket/ViewController.swift
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -7,25 +7,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var pageData: PageInfo?
-    
-    func jsonParsingData() {
-        guard let dataAsset = NSDataAsset(name: "Mock") else {
-            print("error")
-            return
-        }
-        let jsonDecoder = JSONDecoder()
-        
-        do {
-            pageData = try jsonDecoder.decode(PageInfo.self, from: dataAsset.data)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
+    var pageData = jsonParsing()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        jsonParsingData()
         
     }
 }
